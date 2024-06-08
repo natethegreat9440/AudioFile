db7fbd499c5dfd5e6407402452ab0d3ac90f8977
natethegreat9440 <nathan.haley@wsu.edu>
2024-06-07 21:45:08 -0700

    Josh's Visualizer and Edits Added as seperate class files for now

---
+++ AudioFile/Assets/Scripts/GUIControllerJTK.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Song
+{
+    public string name;
+    public AudioClip clip;
+
+    public Song(string name, AudioClip clip)
+    {
+        this.name = name;
+        this.clip = clip;
+    }
+}
+
+public class MediaLibrary : MonoBehaviour
+{
+    public Song[] songs; // Array to store songs
+
+    public void AddSong(string songName, AudioClip clip)
+    {
+        //Create Song
+        Song newSong = new Song(songName, clip);
+
+        //Receate Array
+        int currentLength = songs.Length;
+        Song[] newSongs = new Song[currentLength + 1];
+        songs.CopyTo(newSongs, 0);
+        newSongs[currentLength] = newSong;
+
+        //Update Array Reference
+        songs = newSongs;
+    }
+}
