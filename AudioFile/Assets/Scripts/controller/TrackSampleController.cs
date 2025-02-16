e308c5cfbfb8857aaead6b0e79049b77d9f224b0
natethegreat9440 <nathan.haley@wsu.edu>
2025-02-16 11:37:03 -0800

    Test Project initial set-up

---
+++ AudioFile/Assets/Scripts/controller/TrackSampleController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using AudioFile.Controller;
+
+namespace AudioFile.Controller
+{
+    /// <summary>
+    /// Singleton Track Library Controller in AudioFile used for finding and storing track samples from whosampled.com
+    /// <remarks>
+    /// Members: 
+    /// </remarks>
+    /// <see cref="MonoBehaviour"/>
+    /// <seealso cref="IController"/>
+    /// </summary>
+    public class TrackSampleController : MonoBehaviour, IController
+    {
+        // Lazy<T> ensures that the instance is created in a thread-safe manner
+        private static readonly Lazy<TrackSampleController> _instance = new Lazy<TrackSampleController>(CreateSingleton);
+
+        // Private constructor to prevent instantiation
+        private TrackSampleController() { } //Unity objects should not use constructors with parameters as Unity uses its own lifecycle methods to manage these objects
+
+        public static TrackSampleController Instance => _instance.Value;
+
+        private static TrackSampleController CreateSingleton()
+        {
+            // Create a new GameObject to hold the singleton instance if it doesn't already exist
+            GameObject singletonObject = new GameObject(typeof(TrackSampleController).Name);
+
+            // Add the TrackListController component to the GameObject
+            return singletonObject.AddComponent<TrackSampleController>();
+        }
+
+        public void HandleUserRequest(object request, bool isUndo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AudioFile/Tests/AudioFile.Tests/controller_tests/TrackSamplesController_test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using AudioFile.Controller;
+
+namespace AudioFile.Tests
+{
+    [TestFixture]
+    public class FindTrackOnWhoSampled
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            int expected = 4;
+            int actual = 2 + 2;
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public async Task RealHttpRequest_ReturnsHtmlResponse()
+        {
+            //WhoSampledSearcher searcher = new WhoSampledSearcher();
+            var trackSampleController = TrackSampleController.Instance;
+
+            var result = await searcher.SearchTrackAsync("MF DOOM");
+
+            Assert.IsNotNull(result);
+            Assert.IsNotEmpty(result);
+
+            Console.WriteLine("Real request results: " + string.Join(", ", result));
+        }
+
+    }
+}
+
