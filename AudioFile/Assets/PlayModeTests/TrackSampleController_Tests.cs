f5fd63df3f71554b4dec890c8cc59458f91c45f4
natethegreat9440 <nathan.haley@wsu.edu>
2025-02-16 22:09:54 -0800

    Methods added for requesting info form Whosampled.com (getting 403 forbidden errors)
    Request needs to mimic a real browser request for accurately to hopefully not get blocked by the site otherwise I may have to try an alternate approach with Selenium

--- AudioFile/Assets/PlayModeTests/TrackSampleController_Tests.cs
+++ AudioFile/Assets/PlayModeTests/TrackSampleController_Tests.cs
@@ -24,16 +24,18 @@ namespace AudioFile.Tests
         {
             var trackSampleController = TrackSampleController.Instance;
             // Act: Run the search
-            Task<List<string>> searchTask = trackSampleController.SearchTrackAsync("MF DOOM");
-
+            //Task<List<string>> searchTask = trackSampleController.SearchTrackAsync("MF DOOM", "Doomsday");
+            Task<List<string>> searchTask = trackSampleController.SearchTrackAsync("Doomsday MF Doom");
             // Wait for the async method to complete
             yield return new WaitUntil(() => searchTask.IsCompleted);
 
             // Assert: Ensure we got results
             Assert.IsNotNull(searchTask.Result); //This passes so we get some sort of result
             Debug.Log($"Search Results: {string.Join(", ", searchTask.Result)}"); //Looks like it's just an empty string. Mthod needs refinement to work with actual site HTML structure
-            //Assert.IsNotEmpty(searchTask.Result);
+            Assert.IsNotEmpty(searchTask.Result);
             //Assert.Contains("Rapp Snitch Knishes", searchTask.Result);
         }
+
+        //TODO: Add more test cases for transforming track titles and artist names that don't match casewise with the whosampled.com correpsonding page
     }
 }
